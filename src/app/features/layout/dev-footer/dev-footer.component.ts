c33456e93c7894f96e0dce56f7a6f8f23bacce59
Max Geller <mxgeller@gmail.com>
2024-10-01 13:49:13 -0700

    updating dev footer logic

--- src/app/features/layout/dev-footer/dev-footer.component.ts
+++ src/app/features/layout/dev-footer/dev-footer.component.ts
@@ -5,24 +5,35 @@ import {
   ChangeDetectionStrategy,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import {
+  AuthChangeEvent,
+  AuthSession,
+  Session,
+  User,
+} from '@supabase/supabase-js';
 import { AsyncPipe } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { AuthService } from '../../../core/services/auth.service';
 
 @Component({
   selector: 'app-dev-footer',
   standalone: true,
-  imports: [MatToolbarModule, AsyncPipe, CommonModule],
+  imports: [CommonModule, MatToolbarModule, AsyncPipe],
   templateUrl: './dev-footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class DevFooterComponent implements OnInit {
   environment: string = '';
   showFooter: boolean = false;
+  currentUser: User | null = null;
 
-  constructor(@Inject('ENVIRONMENT') private env: any) {
+  constructor(
+    @Inject('ENVIRONMENT') private env: any,
+    private authService: AuthService
+  ) {
     this.environment = this.env.name;
-    this.showFooter = !this.env.showDevFooter;
+    this.showFooter = this.env.showDevFooter;
+    this.currentUser = this.authService.getCurrentUser();
   }
   ngOnInit() {}
 }
