487972683d3692eb1874bebd92e1c0c869aa3b2f
Max Geller <mxgeller@gmail.com>
2024-10-01 12:35:49 -0700

    updating deployment settings

--- src/app/features/layout/dev-footer/dev-footer.component.ts
+++ src/app/features/layout/dev-footer/dev-footer.component.ts
@@ -1,33 +1,23 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  OnDestroy,
-} from '@angular/core';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AsyncPipe } from '@angular/common';
-
-// Import Angular Material Components
 import { MatToolbarModule } from '@angular/material/toolbar';
-
-// Import Services
 import { AuthService } from '../../../core/services/auth.service';
-
-// Import Interfaces
-import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'app-dev-footer',
   standalone: true,
-  imports: [MatToolbarModule, AsyncPipe],
+  imports: [CommonModule,MatToolbarModule, AsyncPipe],
   templateUrl: './dev-footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DevFooterComponent implements OnInit, OnDestroy {
+export class DevFooterComponent implements OnInit {
+  isDevMode = !environment.production;
+
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
-    // this.currentUser = this.authService.getCurrentUser();
+    // Initialization logic here
   }
-
-  ngOnDestroy(): void {}
 }
--- src/app/features/modules/admin/pages/dashboard/dashboard.page.ts
+++ src/app/features/modules/admin/pages/dashboard/dashboard.page.ts
@@ -1,8 +1,29 @@
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Import Angular Material Components
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+  ],
   templateUrl: './dashboard.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
@@ -10,4 +31,11 @@ export class DashboardPage implements OnInit {
 
   ngOnInit(): void { }
 
+  openInNewTab(url: string): void {
+    window.open(url, '_blank');
+  }
+
+  openStaging(): void {
+    window.open('https://angular-18-staging.netlify.app/', '_blank');
+  }
 }
