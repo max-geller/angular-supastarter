59c0e392ac52cace05de14e80f3b2c9b6bc88a60
Max Geller <mxgeller@gmail.com>
2024-10-03 09:05:42 -0700

    updating users table formatting

--- src/app/features/modules/admin/pages/users/components/users-table/users.table.ts
+++ src/app/features/modules/admin/pages/users/components/users-table/users.table.ts
@@ -1,113 +1,144 @@
 import {
-    ChangeDetectionStrategy,
-    Component,
-    OnInit,
-    ChangeDetectorRef,
-  } from '@angular/core';
-  import { CommonModule } from '@angular/common';
-  import { MatTableModule, MatTableDataSource } from '@angular/material/table';
-  import { MatPaginatorModule } from '@angular/material/paginator';
-  import { MatSortModule } from '@angular/material/sort';
-  import { MatIconModule } from '@angular/material/icon';
-  import { MatDialog } from '@angular/material/dialog';
-  
-  // Import Services
-  import { ToastService } from '../../../../../../../core/services/toast.service';
-  import { AdminService } from '../../../../../../../core/services/admin.service';
-  
-  // Import Models
-  import { UserInterface } from '../../../../../../../core/models/user.model';
+  ChangeDetectionStrategy,
+  Component,
+  AfterViewInit,
+  ChangeDetectorRef,
+} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ViewChild } from '@angular/core';
 
-  // Import Components
-  import { EditUserDialog } from '../edit-user/edit-user.dialog';
-  import { ConfirmDeleteService } from '../../../../../../../core/services/confirm-delete.service';
+// Import Angular Material Components
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
+import { MatPaginatorModule, MatPaginator } from '@angular/material/paginator';
+import { MatSortModule, MatSort } from '@angular/material/sort';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialog } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+// Import Services
+import { ToastService } from '../../../../../../../core/services/toast.service';
+import { AdminService } from '../../../../../../../core/services/admin.service';
+import { ConfirmDeleteService } from '../../../../../../../core/services/confirm-delete.service';
 
-  @Component({
-    selector: 'admin-users-table',
-    standalone: true,
-    imports: [
-      CommonModule,
-      MatTableModule,
-      MatPaginatorModule,
-      MatSortModule,
-      MatIconModule,
-    ],
-    templateUrl: './users.table.html',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-  })
-  export class UsersTableComponent implements OnInit {
-    displayedColumns: string[] = [
-      'id',
-      'email',
-      'first_name',
-      'last_name',
-      'created_at',
-      'is_active',
-      'edit',
-      'delete',
-    ];
-    dataSource = new MatTableDataSource<UserInterface>([]);
-  
-    constructor(
-      private adminService: AdminService,
-      private cdr: ChangeDetectorRef,
-      private dialog: MatDialog,
-      private confirmDeleteService: ConfirmDeleteService,
-      private toastService: ToastService
-    ) {}
-  
-    ngOnInit() {
-      this.loadUsers();
-    }
-  
-    loadUsers() {
-      this.adminService.getAllUsers().subscribe({
-        next: (users) => {
-          console.log('Users received:', users);
-          this.dataSource.data = users;
-          this.cdr.detectChanges();
-        },
-        error: (error) => {
-          console.error('Error fetching users:', error);
-        },
-      });
-    }
-  
-    refreshUsers() {
-      this.loadUsers();
-    }
-  
-    editUser(user: UserInterface) {
-      const dialogRef = this.dialog.open(EditUserDialog, {
-        width: '400px',
-        data: user,
-      });
-  
-      dialogRef.afterClosed().subscribe((result) => {
-        if (result) {
-          this.refreshUsers();
-        }
-      });
-    }
-  
-    deleteUser(user: UserInterface) {
-      this.confirmDeleteService
-        .openAdvancedConfirmDialog(`${user.first_name} ${user.last_name}`, 'user')
-        .subscribe((confirmed) => {
-          if (confirmed) {
-            this.adminService.deleteUser(user.id).subscribe({
-              next: () => {
-                this.toastService.showToast('User deleted successfully');
-                this.refreshUsers();
-              },
-              error: (error) => {
-                console.error('Error deleting user:', error);
-                this.toastService.showToast(
-                  'Error deleting user. Please try again.'
-                );
-              },
-            });
-          }
-        });
+// Import Models
+import { UserInterface } from '../../../../../../../core/models/user.model';
+
+// Import Components
+import { EditUserDialog } from '../edit-user/edit-user.dialog';
+
+@Component({
+  selector: 'admin-users-table',
+  standalone: true,
+  styleUrls: ['./user.table.scss'],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatCardModule,
+  ],
+  templateUrl: './users.table.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UsersTableComponent implements AfterViewInit {
+  displayedColumns: string[] = [
+    'edit',
+
+    'email',
+    'tenant_name',
+    'role_name',
+    'first_name',
+    'last_name',
+    'last_login',
+    'created_at',
+    'is_active',
+    'is_registered',
+    'id',
+  ];
+  dataSource = new MatTableDataSource<UserInterface>([]);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(
+    private adminService: AdminService,
+    private cdr: ChangeDetectorRef,
+    private dialog: MatDialog,
+    private confirmDeleteService: ConfirmDeleteService,
+    private toastService: ToastService
+  ) {}
+
+  ngAfterViewInit() {
+    this.loadUsers();
+    this.dataSource.paginator = this.paginator as MatPaginator;
+    this.dataSource.sort = this.sort as MatSort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
     }
   }
+
+  loadUsers() {
+    this.adminService.getAllUsers().subscribe({
+      next: (users) => {
+        console.log('Users received:', users);
+        this.dataSource.data = users;
+        this.cdr.detectChanges();
+      },
+      error: (error) => {
+        console.error('Error fetching users:', error);
+      },
+    });
+  }
+
+  refreshUsers() {
+    this.loadUsers();
+  }
+
+  editUser(user: UserInterface) {
+    const dialogRef = this.dialog.open(EditUserDialog, {
+      width: '400px',
+      data: user,
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.refreshUsers();
+      }
+    });
+  }
+
+  deleteUser(user: UserInterface) {
+    this.confirmDeleteService
+      .openAdvancedConfirmDialog(`${user.first_name} ${user.last_name}`, 'user')
+      .subscribe((confirmed) => {
+        if (confirmed) {
+          this.adminService.deleteUser(user.id).subscribe({
+            next: () => {
+              this.toastService.showToast('User deleted successfully');
+              this.refreshUsers();
+            },
+            error: (error) => {
+              console.error('Error deleting user:', error);
+              this.toastService.showToast(
+                'Error deleting user. Please try again.'
+              );
+            },
+          });
+        }
+      });
+  }
+}
