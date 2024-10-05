10a5c9afa32e95c8746b0bd4f5281ea0b7140def
Max Geller <mxgeller@gmail.com>
2024-10-05 15:42:27 -0700

    updating user settings page

--- src/app/features/modules/account/pages/settings/settings.page.ts
+++ src/app/features/modules/account/pages/settings/settings.page.ts
@@ -8,13 +8,23 @@ import { Observable, combineLatest, map } from 'rxjs';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatCardModule } from '@angular/material/card';
-
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
 // Import Services
 
 @Component({
   standalone: true,
   selector: 'settings-page',
-  imports: [CommonModule, MatFormFieldModule, MatSelectModule, MatCardModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatDividerModule,
+  ],
   templateUrl: './settings.page.html',
 })
 export class SettingsPage {}
