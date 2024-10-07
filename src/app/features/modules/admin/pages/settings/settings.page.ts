4c29be212495a4d64075ca11a60333278fe6623c
Max Geller <mxgeller@gmail.com>
2024-10-07 15:29:58 -0700

    updating admin app settings page

--- src/app/features/modules/admin/pages/settings/settings.page.ts
+++ src/app/features/modules/admin/pages/settings/settings.page.ts
@@ -1,8 +1,30 @@
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+
+// Import Angular Material Components
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
 
 @Component({
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatInputModule,
+    MatCheckboxModule
+    
+  ],
   templateUrl: './settings.page.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
